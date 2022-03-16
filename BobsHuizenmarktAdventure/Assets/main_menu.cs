619a1e297ee23f69d16bc4db5904d897d0accc6a
Jorre van Merrienboer <20628857+Darth-Ross@users.noreply.github.com>
2022-03-16 21:38:53 +0100

    Aspect ratio scaling, play & exit button functional, added 3D model

--- BobsHuizenmarktAdventure/Assets/main_menu.cs
+++ BobsHuizenmarktAdventure/Assets/main_menu.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class main_menu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    
+    public void start_game()
     {
-        
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void quit_game()
     {
-        
+        Application.Quit();
     }
 }
