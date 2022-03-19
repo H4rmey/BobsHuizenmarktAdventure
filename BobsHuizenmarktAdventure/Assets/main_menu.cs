53b3c49b96a805a3af2cd22cfd396813e704737b
Jorre van Merrienboer <20628857+Darth-Ross@users.noreply.github.com>
2022-03-19 16:45:02 +0100

    Animation plays when pressing start, scene transition when animation is finished

--- BobsHuizenmarktAdventure/Assets/main_menu.cs
+++ BobsHuizenmarktAdventure/Assets/main_menu.cs
@@ -5,7 +5,31 @@ using UnityEngine.SceneManagement;
 
 public class main_menu : MonoBehaviour
 {
-    
+
+    public void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).normalizedTime > 1)
+        {  //If normalizedTime is 0 to 1 means animation is playing, if greater than 1 means finished
+            Debug.Log("not playing");
+            start_game();
+        }
+        else
+        {
+            Debug.Log("playing");
+        }
+    }
+
+    public Animator anim;
+
+    bool isAnimationStatePlaying(Animator anim, int animLayer, string stateName)
+    {
+        if (anim.GetCurrentAnimatorStateInfo(animLayer).IsName(stateName) &&
+                anim.GetCurrentAnimatorStateInfo(animLayer).normalizedTime < 1.0f)
+            return true;
+        else
+            return false;
+    }
+
     public void start_game()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
